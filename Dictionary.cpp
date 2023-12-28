59a38ae964ca7441d31175980cd8de4e6924d0ba
kuchta <kacper.kuchta@here.com>
2023-12-28 19:41:08 +0100

    main

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -1,7 +1,48 @@
+#include "Dictionary.hpp"
+
+#include <fstream>
 #include <iostream>
 
-int main()
-{
+int main(int arguments_size, char** arguments) {
+    if (arguments_size != 2) {
+        std::cout << "Wrong number of arguments!" << std::endl;
+        std::exit(EXIT_FAILURE);
+        return 1;
+    }
+    const char* file_name = arguments[1];
+    // Open a file for reading
+    std::ifstream inputFile(file_name);
+
+    // Check if the file is opened successfully
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1; // Return an error code
+    }
+    Dictionary<std::string, std::string> dictionary(1000);
+    // Read the file line by line
+    std::string first;
+    std::string second;
+    while (inputFile >> first) {
+        inputFile >> second;
+        dictionary.insert(std::pair(first, second));
+    }
+
+    std::vector<std::string> keys;
+    std::string key;
+
+    while (std::cin >> key) {
+        keys.push_back(key);
+    }
+    for (const auto& element: keys) {
+        if (dictionary.find(element)) {
+            std::string value = dictionary[element];
+            std::cout << value << std::endl;
+        } else {
+            std::cout << "-" << std::endl;
+        }
+    }
+    // Close the file
+    inputFile.close();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
